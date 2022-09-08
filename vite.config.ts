1c2711e6a461f52d248f9dd38839177653a21be8
郑旭钡 <gfazba@gmail.com>
2022-09-08 15:17:20 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import { visualizer } from "rollup-plugin-visualizer";
-import { viteMockServe } from "vite-plugin-mock";
 export default defineConfig({
   plugins: [
-    vue(),
     visualizer(),
-    viteMockServe({
-      mockPath: "./src/mock",
-      localEnabled: true,
-      supportTs: true,
-    }),
   ],
   resolve: {
     alias: {
