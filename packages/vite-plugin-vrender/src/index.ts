c81fd43631ef24af54f7d52e955b2dd8455a0758
郑旭钡 <gfazba@gmail.com>
2022-09-14 18:11:00 +0800

    update file

--- packages/vite-plugin-vrender/src/index.ts
+++ packages/vite-plugin-vrender/src/index.ts
@@ -9,6 +9,13 @@ export default function vrenderPlugin(): Plugin {
     name: 'vite:vrender',
     enforce: 'pre',
     transform(code, id, opt) {
+      const vrenderFileRE = /\.vd$/
+      const scriptRE = /\<script\>(\n(.*\n)*)\<\/script\>/
+      const langRE = /\<script\s*(\slang\s*\=\"(\w*?)\")?\s*\>/
+      if (!vrenderFileRE.test(id)) return code
+
+      const script = code.match(scriptRE)
+
 
     }
   }
