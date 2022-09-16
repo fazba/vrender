a39fd6967b130cefae4ccf397688cfcfa151b08a
郑旭钡 <gfazba@gmail.com>
2022-09-16 18:05:11 +0800

    add test file

--- packages/vite-plugin-vrender/src/index.ts
+++ packages/vite-plugin-vrender/src/index.ts
@@ -3,18 +3,26 @@ import type { Plugin, ViteDevServer } from 'vite'
 
 
 export default function vrenderPlugin(): Plugin {
-
+  console.log('pppppppp')
 
   return {
     name: 'vite:vrender',
     enforce: 'pre',
-    transform(code, id, opt) {
+    transform(code, id) {
       const vrenderFileRE = /\.vd$/
+      const groupRE = /\<group\>(\n*.*\n*)*\<\/group\>/
       const scriptRE = /\<script\>(\n(.*\n)*)\<\/script\>/
       const langRE = /\<script\s*(\slang\s*\=\"(\w*?)\")?\s*\>/
+      //
+      console.log({ id })
+      console.log('※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※')
       if (!vrenderFileRE.test(id)) return code
+      console.log({ code, id })
 
+      const group = code.match(groupRE)
       const script = code.match(scriptRE)
+      console.log({ group })
+      console.log({ script })
 
 
     }
